0f0dc1178c54ebbde3adc4dd1ad1c4b5a1bb8cbe
felipe.sulzbach <feliperpdr@yahoo.com>
2023-01-09 22:23:15 -0300

    refactor: create makeSut

--- test/data/usecase/authentication/remote-authentication.spec.ts
+++ test/data/usecase/authentication/remote-authentication.spec.ts
@@ -1,12 +1,25 @@
-import { HttpPostClientSpy } from '../../../resource/moch/moch-http-client'
 import { RemoteAuthentication } from '../../../../src/data/usecase/authentication/remote-authentication'
+import { HttpPostClientSpy } from '../../../resource/moch/moch-http-client';
+
+type SubTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SubTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
 
 describe('data :: usecase :: authentication :: RemoteAuthentication', () => {
   test('Should call HttClient with correct URL', async () => {
 
-    const url = 'any_url'
-    const httpPostClientSpy = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    const url = 'other_url'
+    const { sut, httpPostClientSpy } = makeSut(url);
     await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
   })
